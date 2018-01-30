c8a2380e13aaf65db4eeaa590d6ce31a682f3ef2
Aliaksandr Horski <Aliaksandr_Horski@epam.com>
2018-01-30 12:25:45 +0300

    UnitTests to commit

---
+++ UnitTests/GarbProj/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CSharpCalculator;
+
+namespace ConsoleApp2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Calculator calc = new Calculator();
+			
+			Console.Write(calc.Sub(int.MinValue, int.MaxValue));
+		}
+	}
+}
